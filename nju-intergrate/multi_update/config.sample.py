9aba501a2ff52640ca96af8cf3ef7ffba2208088
Clippit <zlthooray@gmail.com>
2012-03-22 21:32:20 +0800

    seperate secret config from python scripts

---
+++ nju-intergrate/multi_update/config.sample.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+URL_SHORTER_API = 'http://short-example.com/yourls-api.php?signature=xxxxxxxxxx&action=shorturl&format=simply&url='
+
+WORDPRESS_XMLRPC_URL = 'http://example-wordpress.com/xmlrpc.php'
+WORDPRESS_USERNAME = 'username'
+WORDPRESS_PASSWORD = 'password'
+
+DOUBAN_OAUTH_CONSUMER_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+DOUBAN_OAUTH_CONSUMER_TOKEN = 'xxxxxxxxxxxxxxxx'
+DOUBAN_OAUTH_TOKEN_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+DOUBAN_OAUTH_TOKEN_TOKEN = 'xxxxxxxxxxxxxxxx'
+
+RENREN_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+RENREN_SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+RENREN_USERNAME = 'username@example.com'
+RENREN_PASSWORD = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+SINA_APP_KEY = '0000000000'
+SINA_APP_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+SINA_OAUTH_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+SINA_OAUTH_TOKEN_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+SINA_VERIFIER = '000000'
+
